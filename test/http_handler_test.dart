37a3cd40bd1faf6ef3e887548fdc6d6e0b37e960
Arfaz Chhapawala <91971046+Arfaz123@users.noreply.github.com>
2023-11-20 22:57:42 +0530

    initial commit

---
+++ test/http_handler_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:http_handler/http_handler.dart';
+
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
